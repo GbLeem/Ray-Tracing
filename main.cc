28db8eb5960cd1079a4064fd980c872d803a546d
GB_LEEM <juntrex@naver.com>
2022-06-08 20:54:17 +0900

    [DAY2] ~ chapter 6

--- main.cc
+++ main.cc
@@ -1,41 +1,23 @@
+#include "rtweekend.h"
+
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
 
 //for ray-sphere intersection
 //for visualizing normal 
-double hit_sphere(const point3& center, double radius, const ray& r)
-{
-	vec3 oc = r.origin() - center;
-	/*auto a = dot(r.direction(), r.direction());
-	auto b = 2.0 * dot(oc, r.direction());
-	auto c = dot(oc, oc) - radius * radius;
-	auto discriminant = b * b - 4 * a * c;*/
-	auto a = r.direction().length_squared();
-	auto half_b = dot(oc, r.direction());
-	auto c = oc.length_squared() - radius * radius;
-	auto discriminant = half_b * half_b - a * c;
 
-	if (discriminant < 0)
-		return -1.0;
-	else
-		return (-half_b - sqrt(discriminant)) / (2.0 * a);
-}
-
-color ray_color(const ray& r)
+color ray_color(const ray& r, const hittable& world) 
 {
-	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-	if (t > 0.0)
+	hit_record rec;
+	if (world.hit(r, 0, infinity, rec)) 
 	{
-		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-		return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+		return 0.5 * (rec.normal + color(1, 1, 1));
 	}
-
 	vec3 unit_direction = unit_vector(r.direction());
-	t = 0.5 * (unit_direction.y() + 1.0);
-
+	auto t = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -46,6 +28,11 @@ int main()
 	const auto aspect_ration = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ration);
+
+	//World
+	hittable_list world;
+	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 	//Camera
 	const auto viewport_height = 2.0;
@@ -71,11 +58,11 @@ int main()
 			auto v = double(j) / (image_height - 1);
 
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-			color pixel_color = ray_color(r);
+			color pixel_color = ray_color(r, world);
 			write_color(std::cout, pixel_color);
 		}
 	}
-	std::cerr << "\nDone - V2.\n";
+	std::cerr << "\nDone - V3.\n";
 }
 
 
