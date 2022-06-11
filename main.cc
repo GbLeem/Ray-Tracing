e1b4cfeafae084348c9e1422e6a674f99a3c7b04
GB_LEEM <juntrex@naver.com>
2022-06-11 15:39:50 +0900

    [DAY4] ~chapter 10.5

--- main.cc
+++ main.cc
@@ -54,6 +54,7 @@ int main()
 	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
 	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
 	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
 	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 	//Camera
@@ -79,7 +80,7 @@ int main()
 			write_color(std::cout, pixel_color, samplers_per_pixel);
 		}
 	}
-	std::cerr << "\nDone - Glass sphere that sometimes refracts .\n";
+	std::cerr << "\nDone - Schlick Approximation.\n";
 }
 
 
